df1612c3e34d70019e7d1948ac1e011cbc713ec7
Michal Korman <michal.korman@icloud.com>
2023-12-14 14:52:30 +0100

    add test for save and get

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,9 +1,27 @@
+import json
+
 import pytest
 
 from tests.fixtures import flask_app, client
 
 
 @pytest.mark.usefixtures('flask_app', 'client')
-def test_get_items(client):
+def test_save_and_get_items(client):
+    content = 'Test Item #1'
+
+    post_response = client.post(
+        '/api/items',
+        data=json.dumps({
+            'content': content
+        }),
+        content_type='application/json'
+    )
+    assert post_response.status_code == 200
+
     response = client.get('/api/items')
     assert response.status_code == 200
+
+    body = json.loads(response.data)
+    assert len(body) == 1
+    print(body)
+    assert body[0]['content'] == content
