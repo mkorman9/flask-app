b94ceade3662f696e67a898d8f230858ad5ca4bd
Michal Korman <michal.korman@icloud.com>
2023-12-14 15:05:57 +0100

    refactor tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -9,18 +9,27 @@ from tests.fixtures import flask_app, client
 def test_save_and_get_items(client):
     content = 'Test Item #1'
 
-    post_response = client.post(
+    post_response = post_item(client, content)
+    assert post_response.status_code == 200
+
+    get_response, items = get_all_items(client)
+    assert get_response.status_code == 200
+    assert len(items) == 1
+    assert items[0]['content'] == content
+
+
+def get_all_items(client):
+    response = client.get('/api/items')
+    if response.status_code == 200:
+        return response, json.loads(response.data)
+    return response, None
+
+
+def post_item(client, content):
+    return client.post(
         '/api/items',
         data=json.dumps({
             'content': content
         }),
         content_type='application/json'
     )
-    assert post_response.status_code == 200
-
-    response = client.get('/api/items')
-    assert response.status_code == 200
-
-    body = json.loads(response.data)
-    assert len(body) == 1
-    assert body[0]['content'] == content
