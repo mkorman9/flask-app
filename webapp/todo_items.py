2985254acbed75d21ef2113edd22a6232f55bbae
Michal Korman <michal.korman@icloud.com>
2023-12-14 22:40:00 +0100

    refactor

--- webapp/todo_items.py
+++ webapp/todo_items.py
@@ -12,20 +12,18 @@ class TodoItem:
 
 
 def find_todo_items() -> Generator[TodoItem, None, None]:
-    with pool.connection() as connection:
-        with connection.cursor() as cursor:
-            cursor.execute('SELECT id, content from todo_items')
-            for record in cursor.fetchall():
-                yield TodoItem(item_id=record[0], content=record[1])
+    with pool.connection() as conn:
+        with conn.execute('SELECT id, content from todo_items') as result:
+            return (TodoItem(item_id=record[0], content=record[1]) for record in result.fetchall())
 
 
 def add_todo_item(content: str) -> str:
     item_id = uuid7str()
-    with pool.connection() as connection:
-        connection.execute('INSERT INTO todo_items (id, content) VALUES (%s, %s)', (item_id, content))
+    with pool.connection() as conn:
+        conn.execute('INSERT INTO todo_items (id, content) VALUES (%s, %s)', (item_id, content))
         return item_id
 
 
 def delete_all_todo_items():
-    with pool.connection() as connection:
-        connection.execute('DELETE FROM todo_items')
+    with pool.connection() as conn:
+        conn.execute('DELETE FROM todo_items')
