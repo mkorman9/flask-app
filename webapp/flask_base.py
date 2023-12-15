93ebaedcb000ea9b5cb57b130a639cfe374a57dd
Michal Korman <michal.korman@icloud.com>
2023-12-15 11:50:00 +0100

    properly extract field from validation error

--- webapp/flask_base.py
+++ webapp/flask_base.py
@@ -1,4 +1,6 @@
 import traceback
+from functools import reduce
+from typing import Collection
 
 from flask import Flask
 from pydantic import ValidationError
@@ -13,7 +15,11 @@ def create_base_app() -> Flask:
         return {
             'title': 'Provided request body contains schema violations',
             'type': 'ValidationError',
-            'cause': [{'field': '.'.join(e['loc']), 'code': e['type'], 'message': e['msg']} for e in e.errors()]
+            'cause': [{
+                'field': __extract_field(e['loc']),
+                'code': e['type'],
+                'message': e['msg']
+            } for e in e.errors()]
         }, 400
 
     @app.errorhandler(Exception)
@@ -46,3 +52,11 @@ def create_base_app() -> Flask:
         }, e.code
 
     return app
+
+
+def __extract_field(loc: Collection[any]) -> str:
+    return reduce(
+        lambda acc, part: acc + f'[{part}]' if isinstance(part, int) else (part if len(acc) == 0 else f'.{part}'),
+        loc,
+        ''
+    )
