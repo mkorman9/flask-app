8349803b25886ee2aa73919fd8ea0c2358430c73
Michal Korman <michal.korman@icloud.com>
2023-12-16 11:46:37 +0100

    error message on db reconnect failure

--- webapp/db.py
+++ webapp/db.py
@@ -6,10 +6,16 @@ from webapp.config import config
 
 logging.getLogger("psycopg.pool").setLevel('ERROR')
 
+
+def __reconnect_failed(p):
+    print('🚫 Reconnect to the database has failed')
+
+
 pool = ConnectionPool(
     config.DB_URL,
     min_size=config.DB_POOL_MIN,
     max_size=config.DB_POOL_MAX,
     open=False,
-    timeout=10
+    timeout=10,
+    reconnect_failed=__reconnect_failed,
 )
