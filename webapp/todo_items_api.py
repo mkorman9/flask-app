e4d4d81306eb1a9e826add3cd8ea41e5bd6891ee
Michal Korman <michal.korman@icloud.com>
2023-12-16 22:37:54 +0100

    lint

--- webapp/todo_items_api.py
+++ webapp/todo_items_api.py
@@ -18,7 +18,7 @@ class TodoItemModel(BaseModel):
 
 @api.get('/api/items')
 def get_items():
-    page_size = request.args.get('page_size', default=10, type=int)
+    page_size = request.args.get('page_size', type=int, default=10)
     page_token = request.args.get('page_token', type=str)
 
     page = find_todo_items_page(page_size, page_token)
