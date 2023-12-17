ca875438ef6f1871c5ec21afb4df40bb477482da
Michal Korman <michal.korman@icloud.com>
2023-12-17 17:30:34 +0100

    refactor websocket api

--- webapp/websocket_api.py
+++ webapp/websocket_api.py
@@ -1,22 +1,67 @@
+import uuid
+
 from flask_sock import Sock
+from simple_websocket.ws import Base
+
+WS_PATH = '/ws'
+WS_READ_TIMEOUT = 15
+
+
+class WebSocketContext(object):
+    def __init__(self, ws: Base):
+        self.id = uuid.uuid4()
+        self.ws = ws
+        self.to_close = False
+        self.all_clients = {}
+
+    def close(self):
+        self.to_close = True
+
+    def send(self, message: str | bytes):
+        self.ws.send(message)
+
+    def broadcast(self, message: str | bytes):
+        for client in self.all_clients.values():
+            client.send(message)
+
+
+def on_connect(ctx: WebSocketContext):
+    print('websocket connected', ctx.id)
+
+
+def on_message(ctx: WebSocketContext, message: str | bytes):
+    print(f'websocket message ({ctx.id}):', message)
+
+    if message == 'close':
+        ctx.close()
+        return
+
+    ctx.send(message)
+
+
+def on_disconnect(ctx: WebSocketContext):
+    print('websocket disconnected', ctx.id)
 
 
 def register(sock: Sock):
-    @sock.route('/ws')
-    def websocket_endpoint(ws):
-        print('websocket connected')
+    all_clients = {}
 
-        while True:
-            data = ws.receive(timeout=10)
-            if not data:
-                ws.close()
+    @sock.route(WS_PATH)
+    def websocket_endpoint(ws: Base):
+        ctx = WebSocketContext(ws)
+        all_clients[ctx.id] = ctx
+        ctx.all_clients = all_clients
+
+        on_connect(ctx)
+
+        while not ctx.to_close:
+            message = ws.receive(timeout=WS_READ_TIMEOUT)
+            if not message:
                 break
 
-            print('websocket message:', data)
-            if data == 'close':
-                ws.close()
-                break
+            on_message(ctx, message)
 
-            ws.send(data)
+        ws.close()
+        del all_clients[ctx.id]
 
-        print('websocket disconnected')
+        on_disconnect(ctx)
