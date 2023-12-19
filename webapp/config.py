4cd8c89446f88bcbe48da74696385be6135777ef
Michal Korman <michal.korman@icloud.com>
2023-12-19 18:07:45 +0100

    Revert "initialize logger before config"
    This reverts commit c1de2eb06f50a5cb80c16ba1af83a7fe63bfb3cc.

--- webapp/config.py
+++ webapp/config.py
@@ -1,14 +1,13 @@
-import logging
 import os
 import sys
 
 from dotenv import load_dotenv
 from pydantic import ValidationError, BaseModel
 
-from webapp import logger
-
 
 class ConfigModel(BaseModel):
+    LOG_LEVEL: str = 'INFO'
+
     DB_URL: str
     DB_POOL_MIN: int = 1
     DB_POOL_MAX: int = 5
@@ -16,8 +15,7 @@ class ConfigModel(BaseModel):
 
 try:
     load_dotenv()
-    logger.configure_logger(os.getenv('LOG_LEVEL', 'INFO'))
     config = ConfigModel(**os.environ)
 except ValidationError as e:
-    logging.error('🚫 Failed to load configuration', exc_info=e)
+    print('🚫 Failed to load configuration:', e, file=sys.stderr)
     sys.exit(4)
