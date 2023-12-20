d2007afceac772dd7f51b805855b71e9b6e3b475
Michal Korman <michal.korman@icloud.com>
2023-12-20 21:43:03 +0100

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,39 +4,39 @@ from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_for_logs
 
 
-__postgres = DockerContainer('postgres:15')
+_postgres = DockerContainer('postgres:15')
 
 
 def pytest_configure(config):
-    global __postgres
+    global _postgres
 
     db_name = 'test_db'
     db_user = 'test_user'
     db_password = 'test_password'
 
-    __postgres.with_exposed_ports(5432)
-    __postgres.with_env('POSTGRES_DB', db_name)
-    __postgres.with_env('POSTGRES_USER', db_user)
-    __postgres.with_env('POSTGRES_PASSWORD', db_password)
-    __postgres.with_volume_mapping(
+    _postgres.with_exposed_ports(5432)
+    _postgres.with_env('POSTGRES_DB', db_name)
+    _postgres.with_env('POSTGRES_USER', db_user)
+    _postgres.with_env('POSTGRES_PASSWORD', db_password)
+    _postgres.with_volume_mapping(
         f'{os.getcwd()}/migrations',
         '/docker-entrypoint-initdb.d',
         'ro'
     )
 
-    __postgres.start()
+    _postgres.start()
     wait_for_logs(
-        __postgres,
+        _postgres,
         'database system is ready to accept connections'
     )
 
-    host = __postgres.get_container_host_ip()
-    port = __postgres.get_exposed_port(5432)
+    host = _postgres.get_container_host_ip()
+    port = _postgres.get_exposed_port(5432)
     conn_str = f'{db_user}:{db_password}@{host}:{port}/{db_name}'
     os.environ['DB_URL'] = f'postgresql+psycopg://{conn_str}'
 
 
 def pytest_unconfigure(config):
-    global __postgres
+    global _postgres
 
-    __postgres.stop()
+    _postgres.stop()
