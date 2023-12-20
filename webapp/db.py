3b40ee8e39dd542f2213c3beb31e7f5e80045256
Michal Korman <michal.korman@icloud.com>
2023-12-20 01:30:46 +0100

    secure pool creation with a lock

--- webapp/db.py
+++ webapp/db.py
@@ -2,30 +2,33 @@ import atexit
 import logging
 from typing import Optional
 
+from gevent.threading import Lock
 from psycopg_pool import ConnectionPool
 
 from webapp.config import get_config
 
 _pool: Optional[ConnectionPool] = None
+_lock = Lock()
 
 
 def connection():
-    global _pool
+    global _pool, _lock
 
-    if not _pool:
-        logging.getLogger('psycopg.pool').setLevel('ERROR')
+    with _lock:
+        if not _pool:
+            logging.getLogger('psycopg.pool').setLevel('ERROR')
 
-        c = get_config()
-        _pool = ConnectionPool(
-            c.DB_URL,
-            min_size=c.DB_POOL_MIN,
-            max_size=c.DB_POOL_MAX,
-            timeout=10,
-            reconnect_failed=_reconnect_failed,
-            open=True
-        )
+            c = get_config()
+            _pool = ConnectionPool(
+                c.DB_URL,
+                min_size=c.DB_POOL_MIN,
+                max_size=c.DB_POOL_MAX,
+                timeout=10,
+                reconnect_failed=_reconnect_failed,
+                open=True
+            )
 
-        atexit.register(_close_pool)
+            atexit.register(_close_pool)
 
     return _pool.connection()
 
